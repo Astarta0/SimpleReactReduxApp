1d997468e57d509e355fd81fedf52d65c6355780
Anastaia Zaytseva <anastasia.zaytseva0@yandex.ru>
2018-04-21 22:19:04 +0300

    change line separators

--- src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import mainReducer from '../mainReducer/mainReducer';
-import logger from 'redux-logger';
-import { redirect } from '../middlewares/redirect';
-
-// TODO attach redux devtools
-export const store = createStore(mainReducer, applyMiddleware(redirect, logger));
-
-window.store = store;
